858ec3ad1cdb6b26a49ce13bd29174bea564573d
Aline Dias <aline@segdig.com.br>
2018-07-22 23:06:31 -0300

    new challenge

--- Color Maker/ViewController.swift
+++ Color Maker/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var colorView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func changeColorElement(_ sender: Any) {
+        let r: CGFloat = CGFloat(self.redSlider.value)
+        let g: CGFloat = CGFloat(self.greenSlider.value)
+        let b: CGFloat = CGFloat(self.blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
+    
 }
 
